aff0b1bae1f769fadcb283884a4bac4b5e74b8e5
annemfoley <amfoley@stu.naperville203.org>
2020-01-23 15:55:07 -0600

    added ultrasonic code

--- Ultrasonic/src/main/java/frc/robot/Robot.java
+++ Ultrasonic/src/main/java/frc/robot/Robot.java
@@ -1,42 +1,60 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-/*----------------------------------------------------------------------------*/
-/* Ultrasonic sensor demo                                                     */
-/* This code takes the maxSonar EZ sensor and outputs a distance in mmm to    */
-/* the dashboard.                                                             */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.MedianFilter;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
+/**
+ * This is a sample program demonstrating how to use an ultrasonic sensor and
+ * proportional control to maintain a set distance from an object.
+ */
 
 public class Robot extends TimedRobot {
-  
-  private AnalogInput m_US;
-  @Override
-  public void robotInit() {
-    m_US = new AnalogInput(0);
-  }
+  // distance in inches the robot wants to stay from an object
+  private static final double kHoldDistance = 12.0;
 
-  @Override
-  public void teleopInit() {
-  }
+  // factor to convert sensor values to a distance in inches
+  private static final double kValueToInches = 0.125;
 
+  // proportional speed constant
+  private static final double kP = 0.05;
+
+  private static final int kLeftMotorPort = 0;
+  private static final int kRightMotorPort = 1;
+  private static final int kUltrasonicPort = 0;
+
+  // median filter to discard outliers; filters over 10 samples
+  private final MedianFilter m_filter = new MedianFilter(10);
+
+  private final AnalogInput m_ultrasonic = new AnalogInput(kUltrasonicPort);
+  private final DifferentialDrive m_robotDrive
+      = new DifferentialDrive(new PWMVictorSPX(kLeftMotorPort),
+      new PWMVictorSPX(kRightMotorPort));
+
+  /**
+   * Tells the robot to drive to a set distance (in inches) from an object
+   * using proportional control.
+   */
   @Override
   public void teleopPeriodic() {
-    double sensorValue = m_US.getVoltage();
-    final double scaleFactor = 1/(5./1024.); //scale converting voltage to distance
-    double distance = 5*sensorValue*scaleFactor; //convert the voltage to distance
-    SmartDashboard.putNumber("DB/Slider 0", distance); //write the value to the LabVIEW DriverStation
-  
-  }
+    // sensor returns a value from 0-4095 that is scaled to inches
+    // returned value is filtered with a rolling median filter, since ultrasonics
+    // tend to be quite noisy and susceptible to sudden outliers
+    double currentDistance = m_filter.calculate(m_ultrasonic.getValue()) * kValueToInches;
 
+    // convert distance error to a motor speed
+    double currentSpeed = (kHoldDistance - currentDistance) * kP;
+
+    // drive robot
+    m_robotDrive.arcadeDrive(currentSpeed, 0);
+  }
 }
